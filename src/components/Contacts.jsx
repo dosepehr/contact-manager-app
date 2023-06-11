58fa029c3de445b0d9563bc032281b86ac60eb83
dosepehr <dosepehr@gmail.com>
2023-06-11 03:43:59 +0330

    make contacts component

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,19 @@
+import { Contact } from './';
+
+const Contacts = () => {
+    return (
+        <div className='mx-auto sm:px-10 md:px-14 lg:px-28'>
+            <div className='grid md:grid-cols-2 gap-4 grid-cols-1'>
+                <Contact />
+                <Contact />
+                <Contact />
+                <Contact />
+                <Contact />
+                <Contact />
+                <Contact />
+            </div>
+        </div>
+    );
+};
+
+export default Contacts;
