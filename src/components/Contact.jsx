92b2cc6e54706ba1ca775ce2bc6534382419ed66
dosepehr <dosepehr@gmail.com>
2023-06-11 14:45:34 +0330

    fetch contacts from server

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,30 @@
 import { AiOutlineEye } from 'react-icons/ai';
 import { BiEditAlt } from 'react-icons/bi';
 import { BsTrash } from 'react-icons/bs';
-const Contact = () => {
+const Contact = ({ name, mobile, email, photo }) => {
     return (
         <div>
             <div className='bg-MainCurrentline p-5 rounded-lg'>
                 <div className='flex flex-col xl:flex-row space-y-5 justify-between items-center'>
                     <img
-                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThyEKIq_a7eWEwVEoo1aTBQ6gV1KQ4BI8ojEQgnl0ITQ&s'
+                        src={photo || `default.png`}
                         alt=''
-                        className='border border-MainPurple rounded-lg'
+                        className='border border-MainPurple rounded-lg max-w-[150px]'
                     />
                     <div className='flex w-full justify-center '>
                         <div className='flex flex-col rounded-lg border border-MainPurple w-full max-w-[300px]'>
                             <div className='bg-MainForground flex flex-col text-center h-full rounded-lg divide-y divide-MainPurple justify-evenly'>
                                 <div className='flex mx-auto w-full justify-center'>
                                     <p className='ml-2'>نام و نام خانوادگی:</p>
-                                    <p>سپهر</p>
+                                    <p>{name}</p>
                                 </div>
                                 <div className='flex mx-auto w-full justify-center'>
                                     <p className='ml-2'>شماره موبایل:</p>
-                                    <p>سپهر</p>
+                                    <p>{mobile}</p>
                                 </div>
                                 <div className='flex mx-auto w-full justify-center'>
                                     <p className='ml-2'>آدرس ایمیل:</p>
-                                    <p>سپهر</p>
+                                    <p>{email}</p>
                                 </div>
                             </div>
                         </div>
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,18 +1,29 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Contact } from './';
+import { getContacts } from '../services/contactServices';
 
 const Contacts = () => {
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        const fetchContacts = async () => {
+            const { data } = await getContacts();
+            setContacts(data);
+        };
+        fetchContacts();
+    }, []);
     return (
         <div className='mx-auto sm:px-10 md:px-14 lg:px-28'>
-            <Link className='bg-MainPink px-2 py-3 mx-auto block w-fit rounded-md' to='create-contact' >ساخت مخاطب جدید</Link>
+            <Link
+                className='bg-MainPink px-2 py-3 mx-auto block w-fit rounded-md'
+                to='create-contact'
+            >
+                ساخت مخاطب جدید
+            </Link>
             <div className='grid md:grid-cols-2 gap-4 grid-cols-1 mt-5'>
-                <Contact />
-                <Contact />
-                <Contact />
-                <Contact />
-                <Contact />
-                <Contact />
-                <Contact />
+                {contacts.length > 0 &&
+                    contacts.map((contact) => <Contact {...contact} />)}
             </div>
         </div>
     );
