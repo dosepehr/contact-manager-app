24d21447ee41a7b6e3257c1a2fdef48235314b4b
dosepehr <dosepehr@gmail.com>
2023-06-13 02:30:58 +0330

    configure store

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import router from './router/router';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </StrictMode>
 );
