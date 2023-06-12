ece65314bc25ee8d0e05db65f04116935755c918
dosepehr <dosepehr@gmail.com>
2023-06-13 02:31:19 +0330

    config store to use RTK query

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsSlice from '../reducers/contactsSlice';
+import { apiSlice } from '../api/apiSlice';
 const store = configureStore({
     reducer: {
         contacts: contactsSlice,
+        [apiSlice.reducerPath]: apiSlice.reducer,
     },
-
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
